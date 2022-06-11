c35a4e7266c5bf1dc1b9f9f82d9777b3e15c84a7
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2022-06-10 20:42:09 -0500

    Add databaseGateway

---
+++ domain/gateways.go
@@ -0,0 +1,11 @@
+package domain
+
+import "leanpub-app/domain/model"
+
+type DatabaseGateway interface {
+	SaveUser(user *model.User)(*model.User,error)
+	GetUsers()(*[]model.User,error)
+	DeleteUser(id string)error
+	UpdateUser(user *model.User)(*model.User,error)
+	Setup()
+}
