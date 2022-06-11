2db2aa25bfee676d4a9de2d211103d9ce11f45b4
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2022-06-11 16:12:35 -0500

    The main is configured

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"github.com/gorilla/mux"
+	"leanpub-app/app"
+	"log"
+	"net/http"
+)
 
+func main() {
+	application := app.CreateApp()
+	application.Router = mux.NewRouter()
+	application.Setup()
+	http.Handle("/", application.Router)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
