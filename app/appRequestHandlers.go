6f30a85c31568386c33d16d08080cda1313379cb
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2022-06-12 17:06:52 -0500

    Added book handlers

--- app/appRequestHandlers.go
+++ app/appRequestHandlers.go
@@ -128,3 +128,139 @@ func (app Application) UpdateUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type","application/json")
 	w.Write(data)
 }
+
+func (app Application) SaveBook(w http.ResponseWriter, r *http.Request) {
+	var book model.Book
+	err := json.NewDecoder(r.Body).Decode(&book)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	bookSaved, err := app.bookUseCases.SaveBook(&book)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	data, err := json.Marshal(bookSaved)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("content-type","application/json")
+	w.Write(data)
+}
+
+func (app Application) GetBooks(w http.ResponseWriter, r *http.Request) {
+	books, err := app.bookUseCases.GetBooks()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	data, err := json.Marshal(books)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("content-type","application/json")
+	w.Write(data)
+}
+
+func (app Application) GetBookById(w http.ResponseWriter, r *http.Request)  {
+	id := mux.Vars(r)["id"]
+	book, err := app.bookUseCases.GetBookById(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	data, err := json.Marshal(&book)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("content-type","application/json")
+	w.Write(data)
+}
+
+func (app Application) GetBookByAuthor(w http.ResponseWriter, r *http.Request) {
+	authorId := mux.Vars(r)["authorId"]
+	book, err := app.bookUseCases.GetBookByAuthor(authorId)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	data, err := json.Marshal(&book)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("content-type","application/json")
+	w.Write(data)
+}
+
+func (app Application) GetBookByCategory(w http.ResponseWriter, r *http.Request) {
+	category := mux.Vars(r)["category"]
+	book, err := app.bookUseCases.GetBookByCategory(category)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	data, err := json.Marshal(&book)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("content-type","application/json")
+	w.Write(data)
+}
+
+func (app Application) DeleteBook(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	err := app.bookUseCases.DeleteBook(id)
+
+	if err != nil{
+		http.Error(w, err.Error(),http.StatusInternalServerError)
+		return
+	}
+}
+
+func (app Application) UpdateBook(w http.ResponseWriter, r *http.Request) {
+	var book model.Book
+	err := json.NewDecoder(r.Body).Decode(&book)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if book.Id == "" {
+		http.Error(w, "USER_NOT_FOUND", http.StatusBadRequest)
+		return
+	}
+
+	updatedBook, err := app.bookUseCases.UpdateBook(&book)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	data, err := json.Marshal(updatedBook)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("content-type","application/json")
+	w.Write(data)
+}
